d465c8f27dbefc7264c922785e2c9ecc4d10745a
vincentmarinello-sweeney <vmarinello-sweeney@csumb.edu>
2026-04-14 23:36:58 -0700

    Worked on DatabaseManager

--- src/main/java/DatabaseManager.java
+++ src/main/java/DatabaseManager.java
@@ -6,10 +6,9 @@
  * @version 1.0 */
 
 import java.rmi.ServerError;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class DatabaseManager {
@@ -23,6 +22,10 @@ public class DatabaseManager {
 
     private Connection connection;
 
+    /**
+     * Constructor to manage the database
+     * Calls createDatabaseTables method
+     */
     public DatabaseManager(){
         try {
             connection = DriverManager.getConnection(databaseURL);
@@ -42,22 +45,109 @@ public class DatabaseManager {
         }
     }
 
-private void createDatabaseTables() {
-    String sqlQuery1 = "CREATE DATABASE";
-    String sqlQuery2 = """
-        CREATE TABLE IF NOT EXISTS user (
-            username        TEXT PRIMARY KEY AUTOINCREMENT
-            email_address   TEXT FOREIGN KEY NOT NULL
-            password        TEXT NOT NULL
-            isAdmin         INTEGER NOT NULL
-        )
-    """;
+    /**
+     * This creates the tables for the database.
+     */
+    private void createDatabaseTables() {
+        String sqlQuery1 = "CREATE DATABASE";
+        String sqlQuery2 = """
+            CREATE TABLE IF NOT EXISTS user (
+                userID          INTEGER FOREIGN KEY  AUTOINCREMENT
+                username        TEXT PRIMARY KEY
+                email_address   TEXT FOREIGN KEY NOT NULL
+                password        TEXT NOT NULL
+                isAdmin         INTEGER NOT NULL
+            )
+        """;
 
-    try (Statement statement = connection.createStatement()){
-        statement.execute(sqlQuery2);
-    } catch (SQLException e){
-        System.err.println("Creation of tables has failed" + e.getMessage());
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(sqlQuery2);
+        } catch (SQLException e) {
+            System.err.println("Creation of tables has failed" + e.getMessage());
+        }
     }
+
+    public void insertEntries(String name){
+        String sqlQuery200 = "INSERT INTO user (username) VALUES (?)";
+
+        String sqlQuery5 = """
+            INSERT INTO user
+            (Username, Email_Address, Password, Is_User_An_Admin)
+            """;
+
+        String sqlQuery6 = """
+        INSERT INTO user
+        (Username, Email_Address, Password, Is_User_An_Admin)
+          DatabaseManager(username, emailAddress, password, isAdmin);
+        """;
+
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery200) {
+            preparedStatement.setString(1, username);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("Insertion of entries has failed." + e.getMessage());
+}
+        }
+    }
+
+    /**
+     * These are queries to get information for each user from the database.
+     */
+    public List<String> getAllUserInfo() {
+        List <String> userStringList  = new ArrayList<>();
+
+        //String sqlQuery1000 = "SELECT userID FROM user WHERE userID == user.userID";
+
+        String sqlQuery100 =
+                "SELECT * FROM user WHERE userID == user.userID";
+
+        String sqlQuery101 =
+                "SELECT * FROM user WHERE username == user.username";
+
+        String sqlQuery102 =
+                "SELECT * FROM user WHERE emailAddress == user.emailAddress";
+
+        String sqlQuery103 =
+                "SELECT * FROM user WHERE password == user.password";
+
+        try(Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sqlQuery100, sqlQuery101, sqlQuery102, sqlQuery103)){
+            while (resultSet.next()){
+                userStringList.add(resultSet.getString("userID"));
+                userStringList.add(resultSet.getString("username"));
+                userStringList.add(resultSet.getString("emailAddress"));
+                userStringList.add(resultSet.getString("password"));
+            }
+
+        } catch(SQLException e) {
+            System.err.println("Failed to get all user information" + e.getMessage());
+        }
+        return userStringList;
+    }
+
+
+    /**
+     * These are queries for the database.
+     */
+    public void update(int userID) {
+        String sqlUpdate1 -"UPDATE user SET updated = 1 WHERE userID = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sqlUpdate1)) {
+            preparedStatement.setInt(1, userID);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("Update failed" + e.getMessage());
+        }
+    }
+
+    public void deleteAnItem (int userID) {
+        String sqlQueryDelete = "DELETE FROM user WHERE userID = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sqlQueryDelete)) {
+            preparedStatement.setInt(1, userID);
+            preparedStatement.executeUpdate();
+        } catch(SQLException e) {
+            System.err.println("Deletion of item has failed."+e.getMessage());
+    }
+
 
     //String sqlQuery3 = """
     // CREATE TABLE IF NOT EXISTS
@@ -66,29 +156,11 @@ private void createDatabaseTables() {
     //String sqlQuery4 = """
     // CREATE TABLE IF NOT EXISTS
     // """;
-
-    String sqlQuery5 = """
-            INSERT INTO user
-            (Username, Email_Address, Password, Is_User_An_Admin)
-            """;
 }
 
 
-    String sqlQuery6 = """
-            INSERT INTO user
-            (Username, Email_Address, Password, Is_User_An_Admin)
-              DatabaseManager(username, emailAddress, password, isAdmin);
-            """;
-
-
-    String sqlQuery100 = "SELECT * FROM user WHERE username == user.username";
-
-    String sqlQuery101 = "SELECT * FROM user WHERE emailAddress == user.emailAddress";
-
-    String sqlQuery102 = "SELECT * FROM user WHERE password == user.password";
-
-    String sqlQuery103 = "SELECT * FROM user WHERE userID == user.userID";
 
 
 
-}
+
+
