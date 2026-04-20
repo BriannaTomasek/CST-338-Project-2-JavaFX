3324a07f0c2b74712f09c14bda500648023a514f
Ariya Briscoe <ariyabriscoe@gmail.com>
2026-04-19 18:38:13 -0700

    Resolve merge conflicts

--- src/main/java/SceneFactory.java
+++ src/main/java/SceneFactory.java
@@ -2,37 +2,29 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- * Explanation: Explanation: This class creates different scenes based on the SceneType and stage.
- * Steps:
- * Each scene is implemented in its own class (LoginScene.java or Dashboard.java etc.).
- * To add a new scene, create yourScene.java
- * Implement : public static Scene create (Stage, stage).
- * Then add a case to the switch statement in the create method of this class.
- * add the scene type to the SceneType enum in SceneType.java
- *
+ * Explanation: This class creates different scenes based on the SceneType and stage.
+ * Steps: Each scene is implemented in its own class (LoginScene.java or Dashboard.java etc.). To
+ * add a new scene, create yourScene.java Implement : public static Scene create (Stage, stage).
+ * Then add a case to the switch statement in the create method of this class. add the scene type to
+ * the SceneType enum in SceneType.java
  *
  * @author Ariya Briscoe
  * @since 4/11/2026
  */
 public abstract class SceneFactory {
 
-  /**
-   * This method creates a scene based on the given SceneType and stage. It uses a switch statement
-   * @return the scene given
-   */
   public static Scene create(SceneType type, Stage stage) {
     switch (type) {
       case LOGIN:
         return LoginScene.create(stage);
       case REGISTRATION:
         return RegistrationScene.create(stage);
-    //  case USER:
-      //  return UserScene.create(stage);
+      case USER:
+        return UserScene.create(stage);
       case ADMINDASHBOARD:
         return AdminDashboardScene.create(stage);
       default:
         throw new IllegalArgumentException("Invalid scene type: " + type);
     }
   }
-
 }
