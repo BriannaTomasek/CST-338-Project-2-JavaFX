ce7b61eb8bc91eb867a0f4f108696cfabfdbb96d
briannatomasek <tomasebm5515@student.laccd.edu>
2026-04-24 02:06:19 -0700

    Add UserToGameUITest file and testImplementations to build.gradle

---
+++ src/test/java/UserToGameUITest.java
@@ -0,0 +1,13 @@
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.matcher.control.LabeledMatchers.hasText;
+
+public class UserToGameUITest extends ApplicationTest {
+
+    private DatabaseManager db;
+
+}
